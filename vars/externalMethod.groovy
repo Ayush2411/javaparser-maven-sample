92b747b7fff4e9e7e0e4df740aea8283dcf3127f
Ayush Rusia <39836776+Ayush2411@users.noreply.github.com>
2022-07-06 17:47:56 +0530

    Update externalMethod.groovy

--- vars/externalMethod.groovy
+++ vars/externalMethod.groovy
@@ -1,6 +1,7 @@
 // Methods in this file will end up as object methods on the object that load returns.
 def call(String whoAreYou = 'human') {
     echo "Look at this, ${whoAreYou}! You loaded this from another file!"
+    sh 'pwd'
 }
 
 //return this;
